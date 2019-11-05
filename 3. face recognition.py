14d897273b34075b47293c1090cc31c0bbf34b40
beejay777 <bijaypathak55@gmail.com>
2019-11-05 19:25:44 +0545

    before mid evaluation

--- 3. face recognition.py
+++ 3. face recognition.py
@@ -2,28 +2,28 @@
 Real Time Face Recogition
 	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
 	==> LBPH computed model (trained faces) should be on trainer/ dir
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
-
 '''
 
 import cv2
 import numpy as np
 import os 
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')   #load trained model
-cascadePath = "cascade.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+facerecognizer = cv2.face.LBPHFaceRecognizer_create()
+facerecognizer.read('trainer/facetrainer.yml')   #load trained model
+
+gunrecognizer = cv2.face.LBPHFaceRecognizer_create()
+gunrecognizer.read('trainer/guntrainer.yml')
+
+gunCascade = cv2.CascadeClassifier("cascade.xml");
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 #iniciate id counter, the number of persons you want to include
-id = 2 #two persons (e.g. Jacob, Jack)
+id = 3 #two persons (e.g. Jacob, Jack)
 
 
-names = ['', ]  #key in names, start from the second place, leave first empty
+names = ['', 'bijay', '', 'bijay', 'bigyan']  #key in names, start from the second place, leave first empty
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
@@ -47,11 +47,18 @@ while True:
         minSize = (int(minW), int(minH)),
        )
 
+    guns = gunCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
     for(x,y,w,h) in faces:
 
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
 
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        id, confidence = facerecognizer.predict(gray[y:y+h,x:x+w])
 
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
@@ -60,9 +67,26 @@ while True:
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
+
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    for (x, y, w, h) in guns:
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        id, confidence = gunrecognizer.predict(gray[y:y + h, x:x + w])
+
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100):
+            id = 'gun'
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
     
     cv2.imshow('camera',img) 
 
